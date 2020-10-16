7f76d3c6ba42b58bf7c73afbc88086335ba1350a
João Batista <j.batista.romao@gmail.com>
2020-10-16 11:47:52 -0300

    Initial Commit

---
+++ Chomsky_CYK/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chomsky_CYK
+//
+//  Created by Joao Batista on 16/10/20.
+//  Copyright © 2020 Joao Batista. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
