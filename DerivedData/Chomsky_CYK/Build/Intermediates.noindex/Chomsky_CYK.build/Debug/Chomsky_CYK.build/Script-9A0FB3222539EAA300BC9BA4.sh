d6f249e21b31cb5ba44bb35f90cee003f2a12c18
João Batista <j.batista.romao@gmail.com>
2020-10-16 11:53:11 -0300

    first commit

---
+++ DerivedData/Chomsky_CYK/Build/Intermediates.noindex/Chomsky_CYK.build/Debug/Chomsky_CYK.build/Script-9A0FB3222539EAA300BC9BA4.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Type a script or drag a script file from your workspace to insert its path.
+if which swiftlint >/dev/null; then
+  swiftlint
+else
+  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+fi
+
